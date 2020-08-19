c06517b8d4a1026cbac15af70a22a28e60c2f09f
Luan Messias <luanmessias10@gmail.com>
2020-08-19 18:00:35 +0100

    Add first code to compare ReactJS vs React Native

--- mobile-react-native/src/index.js
+++ mobile-react-native/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
 
 export default function App() {
-  return <View />
+  return (
+    <>
+      <StatusBar barStyle='light-content' backgroundColor='#7159C1'></StatusBar>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello World</Text>
+        <Text style={styles.heart}>S2</Text>
+      </View>
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159C1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#FFF',
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+})
