fd5e847281c3e0622be8bcb66eb9da13ddb40dcb
Luan Messias <luanmessias10@gmail.com>
2020-09-03 18:37:43 +0100

    Config ESLint, Editor Config and Prettier

--- first-node-project/src/server.ts
+++ first-node-project/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
@@ -7,5 +7,5 @@ app.get('/', (request, response) => {
 });
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333')
+  console.log('🚀 Server started on port 3333');
 });
