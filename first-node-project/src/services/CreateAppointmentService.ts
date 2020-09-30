23689e9d1b48fe0c786b8a4b4e106f1050c5f63a
Luan Messias <luanmessias10@gmail.com>
2020-09-30 11:44:28 -0300

    Add files about error exception class

--- first-node-project/src/services/CreateAppointmentService.ts
+++ first-node-project/src/services/CreateAppointmentService.ts
@@ -3,6 +3,8 @@ import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   provider_id: string;
   date: Date;
@@ -19,7 +21,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentIsSameDate) {
-      throw Error('This appointment is alredy booked');
+      throw new AppError('This appointment is alredy booked');
     }
 
     const appointment = appointmentsRepository.create({
