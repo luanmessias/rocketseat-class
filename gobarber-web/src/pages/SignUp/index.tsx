508def0bf9427f33390dfe4859f68f3132b39662
Luan Messias <luanmessias10@gmail.com>
2020-10-11 12:07:39 -0300

    Add files about signup class

---
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faca o seu cadastro</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-Mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Cadastrar</Button>
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="a">
+        <FiArrowLeft /> Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
