8fb1f526e82e24c56d6b9101ba831c760c6c14c2
Luan Messias <luanmessias10@gmail.com>
2020-10-12 21:45:04 -0300

    Add files about tooltip create class

--- gobarber-web/src/components/Input/styles.ts
+++ gobarber-web/src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -18,6 +21,12 @@ export const Container = styled.div<ContainerProps>`
   color: #666360;
 
   ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
       border-color: #ff9000;
@@ -29,6 +38,8 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
+
 
   & + div {
     margin-top: 8px;
@@ -46,6 +57,23 @@ export const Container = styled.div<ContainerProps>`
 
     &:placeholder {
       color: #666360;
+    }
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+    &:before {
+      border-color: #c53030 transparent;
     }
   }
 `;
