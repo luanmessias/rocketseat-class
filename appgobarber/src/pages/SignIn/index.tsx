7f8c2d6a197d8d757f16f9e0e1951b7494af2636
Luan Messias <luanmessias10@gmail.com>
2020-10-16 11:53:52 -0300

    Add files about navigation class

---
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Reac from 'react-test-renderer'
+
+import { Container } from './styles'
+
+
+const SignIn: React.FC = () => {
+  return (
+    <Container></Container>
+  )
+}
+
+export default SignIn
