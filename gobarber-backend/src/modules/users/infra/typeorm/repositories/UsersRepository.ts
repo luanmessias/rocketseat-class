8046e6e0445fa8d31f0ebd228ba69a66652e5ae8
Luan Messias <luanmessias10@gmail.com>
2020-10-24 16:04:08 -0300

    Config test files to gitignore

--- gobarber-backend/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ gobarber-backend/src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,8 +1,7 @@
 import { getRepository, Repository } from 'typeorm';
-import User from '@modules/users/infra/typeorm/entities/User';
-
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 class UsersRepository implements IUsersRepository {
   private ormRepository: Repository<User>;
