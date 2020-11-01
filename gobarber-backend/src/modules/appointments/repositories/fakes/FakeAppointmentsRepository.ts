f96bb188a57d94427c716f893acd010118f0f019
Luan Messias <luanmessias10@gmail.com>
2020-11-01 09:41:31 -0300

    Appointments month filter class

--- gobarber-backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ gobarber-backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,9 +1,10 @@
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { getYear, getMonth, isEqual } from 'date-fns';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 import IAppoitnemtsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 
 class AppointmentRepository implements IAppoitnemtsRepository {
   private appointments: Appointment[] = [];
@@ -14,6 +15,22 @@ class AppointmentRepository implements IAppoitnemtsRepository {
     );
 
     return findAppointment;
+  }
+
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(appointment => {
+      return (
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year
+      );
+    });
+
+    return appointments;
   }
 
   public async create({
