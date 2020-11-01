323ce4688b2c511b7bd9f53a086c001be90fb288
Luan Messias <luanmessias10@gmail.com>
2020-11-01 12:32:57 -0300

    List available hours class

--- gobarber-backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ gobarber-backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -4,6 +4,7 @@ import Appointment from '@modules/appointments/infra/typeorm/entities/Appointmen
 import IAppoitnemtsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO ';
 
 class AppointmentRepository implements IAppoitnemtsRepository {
   private ormRepository: Repository<Appointment>;
@@ -33,6 +34,28 @@ class AppointmentRepository implements IAppoitnemtsRepository {
         date: Raw(
           dateFieldName =>
             `to_char(${dateFieldName}, 'MM-YYYY) = '${parsedMonth}-${year}'`,
+        ),
+      },
+    });
+
+    return appointments;
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const parsedDay = String(day).padStart(2, '0');
+    const parsedMonth = String(month).padStart(2, '0');
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          dateFieldName =>
+            `to_char(${dateFieldName}, 'DD-MM-YYYY) = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
     });
--- gobarber-backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ gobarber-backend/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,10 +1,11 @@
 import { uuid } from 'uuidv4';
-import { getYear, getMonth, isEqual } from 'date-fns';
+import { getYear, getMonth, isEqual, getDate } from 'date-fns';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 import IAppoitnemtsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO ';
 
 class AppointmentRepository implements IAppoitnemtsRepository {
   private appointments: Appointment[] = [];
@@ -25,6 +26,24 @@ class AppointmentRepository implements IAppoitnemtsRepository {
     const appointments = this.appointments.filter(appointment => {
       return (
         appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year
+      );
+    });
+
+    return appointments;
+  }
+
+  public async findAllInDayFromProvider({
+    provider_id,
+    day,
+    month,
+    year,
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(appointment => {
+      return (
+        appointment.provider_id === provider_id &&
+        getDate(appointment.date) === day &&
         getMonth(appointment.date) + 1 === month &&
         getYear(appointment.date) === year
       );
