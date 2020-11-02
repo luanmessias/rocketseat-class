1b0ec74d98d9931bfd5089bea3a366e9151b9874
Luan Messias <luanmessias10@gmail.com>
2020-11-02 10:18:03 -0300

    Provider schedule class

--- gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,12 +2,15 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureauthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
+const providerApoointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.get('/me', providerApoointmentsController.index);
 
 export default appointmentsRouter;
