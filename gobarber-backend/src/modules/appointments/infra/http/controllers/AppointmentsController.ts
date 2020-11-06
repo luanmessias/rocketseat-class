09cd13a1c9f1b3aa28240bf45f636c057540f639
Luan Messias <luanmessias10@gmail.com>
2020-11-06 10:16:05 -0300

    Celebrate validation class

--- gobarber-backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ gobarber-backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import { Request, Response } from 'express';
 import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
--- gobarber-backend/src/shared/infra/http/server.ts
+++ gobarber-backend/src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
-
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import { errors } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
@@ -16,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
