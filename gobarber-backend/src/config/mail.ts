50a5f7911076208e5caee9f1147dc27cb90d6668
Luan Messias <luanmessias10@gmail.com>
2020-11-06 16:49:56 -0300

    Amazon SES config class

---
+++ gobarber-backend/src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'luan@luanmessias.com',
+      name: 'Luan Messias',
+    },
+  },
+} as IMailConfig;
