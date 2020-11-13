9a4ed3fc4e01cf442d53670f5911ab9b238cdc7a
Luan Messias <luanmessias10@gmail.com>
2020-11-13 12:46:46 -0300

    Appointment customers list class

--- gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ gobarber-backend/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -27,6 +27,7 @@ class ListProviderAppointmentsService {
     year,
   }: IRequest): Promise<Appointment[]> {
     const cacheKey = `provider-appointments:${provider_id}-${year}-${month}-${day}`;
+
     let appointments = await this.cacheProvider.recover<Appointment[]>(
       cacheKey,
     );
