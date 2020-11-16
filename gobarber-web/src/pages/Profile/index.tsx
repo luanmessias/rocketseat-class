42608eea12426ba382961a06e7988f61a5521bf0
Luan Messias <luanmessias10@gmail.com>
2020-11-16 15:30:39 -0300

    Update information class

--- gobarber-web/src/pages/Profile/index.tsx
+++ gobarber-web/src/pages/Profile/index.tsx
@@ -1,79 +1,124 @@
-import React, { ChangeEvent, useCallback, useRef } from 'react';
+import React, { useCallback, useRef, ChangeEvent } from 'react';
 import { FiMail, FiUser, FiLock, FiCamera, FiArrowLeft } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
 import { useToast } from '../../hooks/toast';
 
-import getValidationerrors from '../../utils/getValidationErrors';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+
 import { Container, Content, AvatarInput, Header } from './styles';
 import { useAuth } from '../../hooks/auth';
 
 interface ProfileFormData {
   name: string;
   email: string;
+  old_password: string;
   password: string;
+  password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const { addToast } = useToast();
   const history = useHistory();
+
   const { user, updateUser } = useAuth();
 
   const handleSubmit = useCallback(
     async (data: ProfileFormData) => {
       try {
         formRef.current?.setErrors({});
+
         const schema = Yup.object().shape({
-          name: Yup.string().required('Nome obrigatorio'),
+          name: Yup.string().required('Nome obrigatório'),
           email: Yup.string()
-            .required('E-mail obrigatorio')
-            .email('digite um email valido'),
-          password: Yup.string().min(6, 'Minimo seis digitos'),
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: val => !!val.length,
+            then: Yup.string().required('Campo obrigatório'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: val => !!val.length,
+              then: Yup.string().required('Campo obrigatório'),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password')], 'Confirmação incorreta'),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post('/users', data);
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
 
-        history.push('/');
+        const formData = {
+          name,
+          email,
+          ...(old_password
+            ? {
+                old_password,
+                password,
+                password_confirmation,
+              }
+            : {}),
+        };
+
+        const response = await api.put('/profile', formData);
+
+        updateUser(response.data);
+
+        history.push('/dashboard');
 
         addToast({
           type: 'success',
-          title: 'Cadastro realizado!',
-          description: 'Voce ja pode fazer o seu logon no GoBarber',
+          title: 'Perfil atualizado!',
+          description:
+            'Suas informações do perfil foram atualizadas com sucesso!',
         });
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
-          const errors = getValidationerrors(err);
+          const errors = getValidationErrors(err);
+
           formRef.current?.setErrors(errors);
+
           return;
         }
+
         addToast({
           type: 'error',
-          title: 'Erro no cadastro',
-          description: 'Ocorreu um erro ao fazer o cadastro, tente novamente',
+          title: 'Erro na atualização',
+          description: 'Ocorreu um erro ao atualizar perfil, tente novamente.',
         });
       }
     },
-    [addToast, history],
+    [addToast, history, updateUser],
   );
 
   const handleAvatarChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
       if (e.target.files) {
         const data = new FormData();
+
         data.append('avatar', e.target.files[0]);
+
         api.patch('/users/avatar', data).then(response => {
           updateUser(response.data);
 
@@ -96,6 +141,7 @@ const Profile: React.FC = () => {
           </Link>
         </div>
       </Header>
+
       <Content>
         <Form
           ref={formRef}
@@ -106,9 +152,10 @@ const Profile: React.FC = () => {
           onSubmit={handleSubmit}
         >
           <AvatarInput>
-            <img src={user.avatar_url} alt="" />
+            <img src={user.avatar_url} alt={user.name} />
             <label htmlFor="avatar">
               <FiCamera />
+
               <input type="file" id="avatar" onChange={handleAvatarChange} />
             </label>
           </AvatarInput>
@@ -117,31 +164,37 @@ const Profile: React.FC = () => {
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input
-            containerStyle={{ marginBottom: '24px' }}
+            containerStyle={{ opacity: 0.5 }}
+            readOnly
+            disabled
             name="email"
             icon={FiMail}
-            placeholder="E-Mail"
+            placeholder="E-mail"
           />
 
           <Input
+            containerStyle={{ marginTop: 24 }}
             name="old_password"
             icon={FiLock}
             type="password"
             placeholder="Senha atual"
           />
+
           <Input
             name="password"
             icon={FiLock}
             type="password"
             placeholder="Nova senha"
           />
+
           <Input
             name="password_confirmation"
             icon={FiLock}
             type="password"
-            placeholder="Confirmar Senha"
+            placeholder="Confirmar senha"
           />
-          <Button type="submit">confirmar alteracoes</Button>
+
+          <Button type="submit">Confirmar mudanças</Button>
         </Form>
       </Content>
     </Container>
