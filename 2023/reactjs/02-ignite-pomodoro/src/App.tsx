10238a9f563c881bf8482322b6f2e54f7e82de5b
Luan <luanmessias03@gmail.com>
2023-05-15 22:00:44 -0300

    P02 - Finish header and nav class

--- 2023/reactjs/02-ignite-pomodoro/src/App.tsx
+++ 2023/reactjs/02-ignite-pomodoro/src/App.tsx
@@ -1,17 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button/index'
+import { Router } from './Router'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyles />
     </ThemeProvider>
   )
